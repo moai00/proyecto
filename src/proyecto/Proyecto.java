1087ca1343841a37464e4e0cbc615229af6d02fa
moai00 <miquelport@gmail.com>
2016-06-16 13:27:28 +0200

    agregados packages dao, modelo, vista

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -5,7 +5,7 @@ package proyecto;
 
 /**
  *
- * @authors Mport y SGamarra
+ * @authors Mport y SGamarra 
  */
 public class Proyecto {
 
@@ -13,6 +13,7 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String hola;
         
     }
     
