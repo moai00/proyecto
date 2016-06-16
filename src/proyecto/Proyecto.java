8f99606355be04afcf8e3a54ec3e0b355507b3ac
moai00 <miquelport@gmail.com>
2016-06-16 11:36:53 +0200

    nuevo proyecto

---
+++ src/proyecto/Proyecto.java
@@ -0,0 +1,19 @@
+/*
+ * 
+ */
+package proyecto;
+
+/**
+ *
+ * @author usu21
+ */
+public class Proyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
