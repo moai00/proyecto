d52530ba63b518649b40a8be98857985e633f14a
moai00 <miquelport@gmail.com>
2016-06-16 13:17:36 +0200

    primer commit del proyecto

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -13,7 +13,7 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
