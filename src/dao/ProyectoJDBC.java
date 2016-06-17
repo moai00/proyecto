321cae1b54e7d7816c74026fb557c6eb177b6d7a
moai00 <miquelport@gmail.com>
2016-06-17 10:02:24 +0200

    uop

--- src/dao/ProyectoJDBC.java
+++ src/dao/ProyectoJDBC.java
@@ -5,8 +5,10 @@
  */
 package dao;
 
+import exceptions.MyException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,26 +19,61 @@ import java.util.logging.Logger;
  */
 public class ProyectoJDBC {
 
-    private Connection connection;
+    private Connection conexion;
+    
+    
+    public boolean insertarUsuario (User usuario) throws MyException{
+        conectar();
+        if (conexion != null){
+        try{
+            String insert = "insert into user values (?,?,?,?,?)";
+            PreparedStatement ps = conexion.prepareStatement(insert);
+            ps.setString(1, usuario.getNif());
+            ps.setString (2, usuario.getNombre());
+            ps.setString(3, usuario.getApellidos());
+            ps.setString(4, usuario.getPeso());
+            ps.setString(5, usuario.getEdad());
+            ps.executeUpdate();
+            ps.close();
+            return true;
+        }catch (SQLException ex){
+            System.out.println("Error al insertar " + ex.getMessage());
+            return false;
+        }finally{
+            desconectar();
+        }
+        }else{
+            return false;
+        }
+    }
+    
+    public void existeUsuario (User u) throws MyException{
+        conectar();
+        try{
+            String query = "select * from user where nif='" + u.getNif()+ "';";
+            Statement st
+        }
+    }
+        
+    
 
-    private void conectar(){
-        try {
+    private void conectar() throws MyException{
+        
             String url = "jdbc:mysql://localhost:3306/proyecto";
             String user = "root";
             String pass = "jeveris";
-
-            connection = DriverManager.getConnection(url, user, pass);
+            try{
+            conexion = DriverManager.getConnection(url, user, pass);
         } catch (SQLException ex) {
-            System.out.println("Error al conectar " + ex.getMessage());
-            connection = null;
+            throw new MyException("Error al conectar " + ex.getLocalizedMessage());
         }
     }
 
-    private void desconectar(){
+    private void desconectar() throws MyException{
         try {
-            connection.close();
+            conexion.close();
         } catch (SQLException ex) {
-            System.out.println("Error al desconectar " + ex.getMessage());
+           throw new MyException("Error al desconectar " + ex.getLocalizedMessage());  
         }
     }
 
